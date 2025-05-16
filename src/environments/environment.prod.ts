d60c48cafaf56cadef13ffb52fbe35ddc6aadea1
Damien Zinsou PADONOU <39190915+predator229@users.noreply.github.com>
2025-05-16 17:31:47 +0300

    update server endpoint for obfucation and cors policy probl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  baseUrl: 'http://server.cmpharma.fr/admin/api/', // Production API
+  baseUrl: '/server/admin/api/', // Production API
 };
