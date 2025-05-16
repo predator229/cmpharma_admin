ba1ee167ee8c6cf23fa0802f758a6d6ba6df6a21
Damien Zinsou PADONOU <39190915+predator229@users.noreply.github.com>
2025-05-16 15:38:16 +0300

    Update environment.preprod.ts

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  baseUrl: 'http://cmpharma.cybersds.fr/server/admin/api/', // Preprod API
+  baseUrl: 'http://cmpharmaserver.cybersds.fr/admin/api/', // Preprod API
 };
